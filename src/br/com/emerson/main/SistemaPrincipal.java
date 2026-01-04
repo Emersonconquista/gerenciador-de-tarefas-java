823dcda9b4ccec1b63aeaea8aa2c50e990e878d0
Emerson Conquista <emerson.conquista71@gmail.com>
2026-01-04 08:13:05 -0300

    Adicionada função de remover e concluir tarefas

--- src/br/com/emerson/main/SistemaPrincipal.java
+++ src/br/com/emerson/main/SistemaPrincipal.java
@@ -2,7 +2,7 @@ package br.com.emerson.main;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import br.com.emerson.model.Tarefa; // Verifique se esta linha existe!
+import br.com.emerson.model.Tarefa;
 
 public class SistemaPrincipal {
     public static void main(String[] args) {
@@ -10,25 +10,44 @@ public class SistemaPrincipal {
         Scanner leitor = new Scanner(System.in);
         int opcao = 0;
 
-        while (opcao != 3) {
+        while (opcao != 5) {
             System.out.println("\n--- GERENCIADOR DO EMERSON ---");
             System.out.println("1 - Adicionar Tarefa");
             System.out.println("2 - Listar Tarefas");
-            System.out.println("3 - Sair");
+            System.out.println("3 - Marcar como Concluída");
+            System.out.println("4 - Remover Tarefa");
+            System.out.println("5 - Sair");
             System.out.print("Escolha: ");
             
             opcao = leitor.nextInt();
-            leitor.nextLine(); // Limpa o teclado
+            leitor.nextLine(); 
 
             if (opcao == 1) {
                 System.out.print("O que precisa fazer? ");
                 String desc = leitor.nextLine();
                 lista.add(new Tarefa(desc));
                 System.out.println("Salvo com sucesso!");
+
             } else if (opcao == 2) {
                 System.out.println("\nSUA LISTA:");
-                for (Tarefa t : lista) {
-                    System.out.println(t);
+                for (int i = 0; i < lista.size(); i++) {
+                    System.out.println(i + " - " + lista.get(i));
+                }
+
+            } else if (opcao == 3) {
+                System.out.print("Digite o número da tarefa concluída: ");
+                int index = leitor.nextInt();
+                if (index >= 0 && index < lista.size()) {
+                    lista.get(index).setConcluida(true);
+                    System.out.println("Tarefa atualizada!");
+                }
+
+            } else if (opcao == 4) {
+                System.out.print("Digite o número da tarefa para remover: ");
+                int index = leitor.nextInt();
+                if (index >= 0 && index < lista.size()) {
+                    lista.remove(index);
+                    System.out.println("Tarefa removida!");
                 }
             }
         }
